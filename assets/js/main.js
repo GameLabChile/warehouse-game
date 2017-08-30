a1ef10ab539ffd9ffdfa2db47523ce187701ffa8
maribel <maribel.diaz.j@gmail.com>
2017-08-30 07:01:30 -0300

    pantalla reuniones

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    setTimeout(function(){
+    	$(".globo").fadeIn(500);
+    },1000);
+
+     setTimeout(function(){
+    	$(".globo2").fadeIn(1000);
+    },2000);
+
+      setTimeout(function(){
+    	$(".globo3").fadeIn(1500);
+    },3000);
+
+       setTimeout(function(){
+    	$(".all-maribel").fadeIn(2000);
+    },4000);
+
+});
